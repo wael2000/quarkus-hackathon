fab5dd0fe341bf348eb967091f1d6058c5a8979a
rhpam <weldoami@redhat.com>
2020-07-02 22:07:32 +0400

    README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # quarkus-hackathon
+Microservices baded Medical case submission portal
+# components
+1 - Physicians: physician profile service connecting to Mongodb 
+2 - Cases: case request service connecting to Postgresql
+3 - bp: kogito based business process service
+4 - Composite: a simple composite service using proxy and microgateway design patterns
+5 - UI: AngularJS
---
+++ push-images.sh
@@ -0,0 +1,5 @@
+#physicians/mvnw clean package -Dquarkus.container-image.push=true -f physicians/pom.xml
+cases/mvnw clean package -Dquarkus.container-image.push=true -f cases/pom.xml
+bp/mvnw clean package -Dquarkus.container-image.push=true -f bp/pom.xml
+composite/mvnw clean package -Dquarkus.container-image.push=true -f composite/pom.xml
+ui/mvnw clean package -Dquarkus.container-image.push=true -f ui/pom.xml
